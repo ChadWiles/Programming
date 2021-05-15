fa641a8fd56370bbeace31f54e209d95a8ed0d1d
ChadWiles <103564240@student.swin.edu.au>
2021-05-16 00:39:38 +1000

    Variables

--- VariablesTask/Program.cs
+++ VariablesTask/Program.cs
@@ -4,9 +4,15 @@ namespace VariablesTask
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            string firstname = "Chad";
+            string lastname = "Wiles";
+            int ID = 103564240;
+            int DOB = 1994;
+            int YEAR = 2021; 
+            firstname = Console.ReadLine();
+            lastname = Console.ReadLine();
+            Console.WriteLine("firstname + lastname");
         }
     }
 }
