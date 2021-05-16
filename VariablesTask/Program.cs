5855b3e4f09d5a8021b27885e2323a0f9b8ab162
ChadWiles <103564240@student.swin.edu.au>
2021-05-16 11:54:14 +1000

    variables

--- VariablesTask/Program.cs
+++ VariablesTask/Program.cs
@@ -5,21 +5,17 @@ namespace VariablesTask
     class Program
     {
         static void Main(string[] args) {
-            string word_1;
-            string word_2;
+            Console.WriteLine("Enter name");
 
-            word_1 = "10";
-            word_2 = "20";
+            string Name = Console.ReadLine();
 
-            int num_1;
-            int num_2; 
+            Console.WriteLine("First name is " + Name);
 
-            //these two lines convert word_1 and word_2 into ints and assign to num_1 and num_2 respectively
-            num_1 = int.Parse(word_1);
-            num_2 = int.Parse(word_2);
+            Console.WriteLine("Enter surname");
+            
+            string Lastname = Console.ReadLine();
 
-            Console.WriteLine(num_1 + num_2);
-
+            Console.WriteLine("Last name is" + Lastname);
          }
     }
 }
