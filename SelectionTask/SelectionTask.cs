05a07a64e2b128fa0b5b7386031ad122eb080cde
ChadWiles <103564240@student.swin.edu.au>
2021-05-16 12:54:27 +1000

    SelectionTask

---
+++ SelectionTask/SelectionTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your age: ");
+            string age = Console.ReadLine();
+
+            int ageNum = int.Parse(age);
+
+            if (ageNum >= 17) {
+                Console.WriteLine("Legally adult age");
+
+            } else
+                {
+                    Console.WriteLine("Legally not adult age");
+                }
+            }
+
+        }
+    }
+}
