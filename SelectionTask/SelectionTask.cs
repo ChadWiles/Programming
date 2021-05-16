b5cac43edaf58600dd2a57633a5c633b4b549a31
ChadWiles <103564240@student.swin.edu.au>
2021-05-16 13:05:47 +1000

    Update

--- SelectionTask/SelectionTask.cs
+++ SelectionTask/SelectionTask.cs
@@ -18,8 +18,22 @@ namespace SelectionTask
                 {
                     Console.WriteLine("Legally not adult age");
                 }
-            }
+            
+            Console.Write("Please enter your gender: ");
+            string gender = Console.ReadLine();
+            
+            Console.Write("Please enter your age: ");
+            
+            int ageNum = int.Parse(age);
 
+            if (ageNum >= 17) {
+                Console.WriteLine(gender + "is an adult");
+
+            } else {
+                Console.WriteLine(gender + "is not an adult");
+            }
+            }
+            
         }
     }
-}
+
