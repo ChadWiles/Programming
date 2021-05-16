1201ebbc2eba577ab349c2e685f3200ff755fb2f
ChadWiles <103564240@student.swin.edu.au>
2021-05-16 12:15:40 +1000

    Pls Work

--- VariablesTask/Program.cs
+++ VariablesTask/Program.cs
@@ -12,17 +12,37 @@ namespace VariablesTask
 
             Console.WriteLine("First name is " + Name);
 
+
+
             Console.WriteLine("Enter surname");
             
             string Lastname = Console.ReadLine();
 
             Console.WriteLine("Last name is " + Lastname);
 
+
+
             Console.WriteLine("Enter StudentID");
 
             string ID = Console.ReadLine();
 
             Console.WriteLine(Name + "'s StudentID is " + ID);
+
+
+
+            Console.WriteLine("Enter Date of Birth");
+
+            string YOB = Console.ReadLine();
+
+            Console.WriteLine(Name + "'s Year of Birth is " + YOB);
+
+
+
+            Console.WriteLine("What year is it?");
+
+            string Year = Console.ReadLine();
+
+            Console.WriteLine("The year is " + Year);            
 
             
          }
