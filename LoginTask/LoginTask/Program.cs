2211e7a96aad2d705f710a9b44149f9627d44f61
ChadWiles <103564240@student.swin.edu.au>
2021-06-10 09:35:00 +1000

    push

---
+++ LoginTask/LoginTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoginTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please log in");
+            Console.WriteLine("Enter username: ");
+
+            string user = Console.ReadLine();
+
+            Console.WriteLine = ("Username is: " + user);
+
+            if ( user = )
+        }
+    }
+}
